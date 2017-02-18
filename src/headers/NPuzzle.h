1d5391ec10687b5a0aab169a3a4b13b7bec03dfa
yat0 <brunoccast@tuta.io>
2017-02-18 18:54:34 +0000

    NPuzzle header minimal changes

--- src/headers/NPuzzle.h
+++ src/headers/NPuzzle.h
@@ -1,26 +1,29 @@
 #ifndef NPUZZLE_H
 #define NPUZZLE_H
 
+#include "Configuration.h"
 using namespace std;
 
 class NPuzzle
 {
 public:
-    NPuzzle(int N);
+    NPuzzle(int);
     ~NPuzzle();
-
-    /* util */
-    void init();
-    bool isSolvable() const;
 
     /* search methods */
     void Astar() const;
     void greedy() const;
+
+    /* util */
+    bool isSolvable() const;
     
 private:
     int N;
     Configuration *original;
     Configuration *goal;
+
+    /* methods */
+    void init();
 };
 
 #endif
